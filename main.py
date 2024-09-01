6647c95f98a0095472b13255eee5b5390eae1093
FREDYK1 <fkwekukankam@gmail.com>
2024-09-01 22:56:09 +0000

    Add Subject and links to the email

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from abc import abstractstaticmethod
 import requests
 from email_implementation import send_email
 
+topic = "wsj.com"
+
 api_key = "487045a82f2649fda9dd43d48c8a77d3"
-url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=487045a82f2649fda9dd43d48c8a77d3"
+url = ("https://newsapi.org/v2/everything?"
+       f"domains={topic}"
+       "&apiKey=487045a82f2649fda9dd43d48c8a77d3"
+       "&language=en")
 
 request = requests.get(url)
 content = request.json()
@@ -12,8 +17,11 @@ content = request.json()
 article_string = ""
 
 for article in content["articles"]:
-    if article['title'] and article["description"]is not None:
-        article_string += article["title"] + "\n" + article["description"] + 2*"\n"
+    if article['title'] and article["description"] is not None:
+        article_string +=("Subject: Today's news "
+                         + "\n" + article["title"]
+                         + "\n" + article["description"]
+                         + "\n" + article["url"] +  2*"\n")
 
 
 send_email(article_string)
